d5c7d8bbdc745204b98b6c109bfe9e646a01676a
aung-kaung-sett <56989378+aung-kaung-sett@users.noreply.github.com>
2019-10-25 10:28:16 +0630

    Separated a function, Added time and space complexity
    Similar question is on https://leetcode.com/problems/valid-parentheses/

--- src/B.java
+++ src/B.java
@@ -1,80 +1,46 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-/*Problem B: Brace Matching*/
+/*Problem B: Brace Matching
+  Time Complexity : O(n)
+  Space Complexity: O(n)
+  where n is the length of the input
+*/
 
-public class B {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-
-        while (T-- > 0) {
-            String input = sc.next();
-
-            Stack<Character> characters = new Stack<>();
-
-            boolean b = true;
-
-            for (int i = 0; i < input.length(); i++) {
-
-                char ch = input.charAt(i);
-
-                if (ch == '(')
-                    characters.add(ch);
-                else if (ch == '{')
-                    characters.add(ch);
-                else if (ch == '[')
-                    characters.add(ch);
-                else if (ch == ')') {
-
-                    if (!characters.isEmpty()) {
-
-                        if (characters.pop() != '(') {
-                            b = false;
-                            break;
-                        }
-
-                    } else {
-                        b = false;
-                        break;
-                    }
-                } else if (ch == '}') {
-                    if (!characters.isEmpty()) {
-
-                        if (characters.pop() != '{') {
-                            b = false;
-                            break;
-                        }
-
-                    } else {
-                        b = false;
-                        break;
-                    }
-
-                } else if (ch == ']') {
-                    if (!characters.isEmpty()) {
-
-                        if (characters.pop() != '[') {
-                            b = false;
-                            break;
-                        }
-
-                    } else {
-                        b = false;
-                        break;
-                    }
-                }
-            }
-
-
-            if (!b || !characters.isEmpty()) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
-        }
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      String input = sc.next();
+      if (isValid(input)) {
+        System.out.println("YES");
+      } else {
+        System.out.println("NO");
+      }
     }
+    sc.close();
+  }
+
+  public static boolean isValid(String s) {
+    Stack<Character> st = new Stack();
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        st.push(')');
+      } else if (c == '{') {
+        st.push('}');
+      } else if (c == '[') {
+        st.push(']');
+      } else if (c == ')' || c == '}' || c == ']') {
+        if (st.isEmpty() || c != st.pop()) {
+          return false;
+        }
+      }
+    }
+    if (st.isEmpty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
